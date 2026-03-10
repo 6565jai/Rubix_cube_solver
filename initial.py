2c0c2f16b4cd3dfc43d920d71ae27b15c0f3a214
6565jai <miglanijai2006@gmail.com>
2026-03-10 16:44:09 +0530

    commit

--- initial.py
+++ initial.py
@@ -43,8 +43,31 @@ def process_roi(frame, start_x, start_y, end_x, end_y):
     # cv2.imshow("Gray ROI", gray_roi)
     # cv2.imshow("HSV ROI", hsv_roi)
     return hsv_roi
+def classify_cube_color(hsv):
+    h, s, v = hsv
+    if s < 40 and v > 150:
+        return "white"
+    if (0 <= h <= 10) or (170 <= h <= 180):
+        return "red"
+    if 10 < h <= 20:
+        return "orange"
+    if 20 < h <= 35:
+        return "yellow"
+    if 35 < h <= 85:
+        return "green"
+    if 85 < h <= 130:
+        return "blue"
 
-
+    return "unknown"
+def draw_labels(frame, start_x, start_y, cell, cells_color):
+    for row in range(3):
+        for col in range(3):
+            text = cells_color[row][col]
+            x = start_x + col * cell + 10
+            y = start_y + row * cell + 30
+            cv2.putText(frame, text, (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (255, 255, 255), 1)
 screen_height = get_screen_height()
 
 cam = cv2.VideoCapture(0)
@@ -60,7 +83,9 @@ while True:
 
     frame, start_x, start_y, end_x, end_y = draw_grid(frame, GRID_SIZE)
     cell = GRID_SIZE // 3
+
     hsv_roi=process_roi(frame, start_x, start_y, end_x, end_y)
+    
     cell = GRID_SIZE // 3
     cells_color = [[None for _ in range(3)] for _ in range(3)]
 
@@ -72,11 +97,15 @@ while True:
             x2 = (col + 1) * cell
             cell_roi = hsv_roi[y1:y2, x1:x2]
             avg_hsv = np.mean(cell_roi, axis=(0,1))
-            cells_color[row][col] = avg_hsv
+            cells_color[row][col] = classify_cube_color(avg_hsv)
     
+    draw_labels(frame, start_x, start_y, cell, cells_color)
+    cv2.imshow("Frame",frame)
+    cv2.imshow("Hsv",hsv_roi)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+for row in cells_color:
+        print(row)
 cam.release()
 cv2.destroyAllWindows()
 
